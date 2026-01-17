7c6237acb3d755fa543060e05e5e870b693cf3e0
cu5tmtp <136180264+cu5tmtp@users.noreply.github.com>
2026-01-17 21:33:17 +0100

    add quests, new recipe

---
+++ kubejs/server_scripts/gregTech/UHVchanges/multiblockRecipesUHV/enhancedfr.js
@@ -0,0 +1,15 @@
+ServerEvents.recipes((event) => {
+
+    event.recipes.gtceu.advanced_fusion("getneutroniumfaster")
+        .inputFluids(
+            'gtceu:americium 1000',
+            'gtceu:naquadria 1000'
+        )
+        .outputFluids(
+            'gtceu:neutronium 2000'
+        )
+        .EUt(GTValues.VA[GTValues.UHV])
+        .duration(200)
+        .addData('heat_level', 2500)
+
+});
