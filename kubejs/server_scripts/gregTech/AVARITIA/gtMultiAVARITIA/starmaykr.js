0e605470e1546d52b72d35ac1144a0af1b5fc1ba
cu5tmtp <136180264+cu5tmtp@users.noreply.github.com>
2026-03-26 09:58:10 +0100

    nerf infinity ingot recipe

--- kubejs/server_scripts/gregTech/AVARITIA/gtMultiAVARITIA/starmaykr.js
+++ kubejs/server_scripts/gregTech/AVARITIA/gtMultiAVARITIA/starmaykr.js
@@ -2,84 +2,84 @@ ServerEvents.recipes((event) => {
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr1")
         .itemInputs(
-            '500x #gtceu:circuits/lv'
+            '350x #gtceu:circuits/lv'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:lvcircuitsingularity"}')
         )
         .duration(1000)
         .EUt(GTValues.VA[GTValues.UEV])
-        .addData('weight', 350)
+        .addData('weight', 50)
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr2")
         .itemInputs(
-            '450x #gtceu:circuits/mv'
+            '300x #gtceu:circuits/mv'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:mvcircuitsingularity"}')
         )
         .duration(1000)
         .EUt(GTValues.VA[GTValues.UEV])
-        .addData('weight', 300)
+        .addData('weight', 50)
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr3")
         .itemInputs(
-            '400x #gtceu:circuits/hv'
+            '250x #gtceu:circuits/hv'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:hvcircuitsingularity"}')
         )
         .duration(1000)
         .EUt(GTValues.VA[GTValues.UEV])
-        .addData('weight', 250)
+        .addData('weight', 50)
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr4")
         .itemInputs(
-            '350x #gtceu:circuits/ev'
+            '200x #gtceu:circuits/ev'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:evcircuitsingularity"}')
         )
         .duration(1000)
         .EUt(GTValues.VA[GTValues.UEV])
-        .addData('weight', 200)
+        .addData('weight', 50)
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr5")
         .itemInputs(
-            '300x #gtceu:circuits/iv'
+            '150x #gtceu:circuits/iv'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:ivcircuitsingularity"}')
         )
         .duration(1000)
         .EUt(GTValues.VA[GTValues.UEV])
-        .addData('weight', 150)
+        .addData('weight', 50)
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr6")
         .itemInputs(
-            '250x #gtceu:circuits/luv'
+            '100x #gtceu:circuits/luv'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:luvcircuitsingularity"}')
         )
         .duration(1000)
         .EUt(GTValues.VA[GTValues.UEV])
-        .addData('weight', 100)
+        .addData('weight', 50)
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr7")
         .itemInputs(
-            '200x #gtceu:circuits/zpm'
+            '75x #gtceu:circuits/zpm'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:zpmcircuitsingularity"}')
         )
         .duration(1000)
         .EUt(GTValues.VA[GTValues.UEV])
-        .addData('weight', 75)
+        .addData('weight', 50)
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr8")
         .itemInputs(
-            '150x #gtceu:circuits/uv'
+            '50x #gtceu:circuits/uv'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:uvcircuitsingularity"}')
@@ -90,14 +90,14 @@ ServerEvents.recipes((event) => {
 
     event.recipes.gtceu.star_maykr_singularities("starmaykr9")
         .itemInputs(
-            '100x #gtceu:circuits/uhv'
+            '25x #gtceu:circuits/uhv'
         )
         .itemOutputs(
             Item.of('avaritia:singularity', '{Id:"avaritia:uhvcircuitsingularity"}')
         )
         .duration(1000)
         .EUt(GTValues.VA[GTValues.UEV])
-        .addData('weight', 25)
+        .addData('weight', 50)
 
     event.remove('avaritia:record_fragment')
     event.recipes.gtceu.star_maykr_singularities("starmaykr10")
